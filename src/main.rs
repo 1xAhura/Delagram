ae9a72a75475d87ad618ca0e66fffbbf6c821842
Ahura Majidi <78syntax@gmail.com>
2026-08-20 17:26:28 +0330

    feat: set telegram token via terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    let mut set_token = false;
+    for argument in std::env::args() {
+        if argument == "--set-token" {
+            set_token = true;
+        }
+
+        if set_token {
+            let contents = format!("TELOXIDE_TOKEN={argument}");
+            std::fs::write("./.env", contents)?;
+        }
+    }
+
+    Ok(())
 }
